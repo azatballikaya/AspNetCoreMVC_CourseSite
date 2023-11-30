e6e01a8da15b73fb62da800b669979c786265e28
azatballikaya <azat.bal@hotmail.com>
2023-11-30 13:10:53 +0300

    Course List and Course Delete
    Course List and Course Delete ActionResults and Views created.

--- efCoreApp/Controllers/OgrenciController.cs
+++ efCoreApp/Controllers/OgrenciController.cs
@@ -9,6 +9,10 @@ namespace efCoreApp.Controllers{
         public OgrenciController(DataContext context){
             _context=context;
         }
+          public async Task<IActionResult> Index(){
+            var ogrenciler= await _context.Ogrenciler.ToListAsync();
+            return View(ogrenciler);
+        }
 
         [HttpGet]
         public IActionResult Create(){
@@ -20,9 +24,65 @@ namespace efCoreApp.Controllers{
            await _context.SaveChangesAsync();
             return RedirectToAction("Index","Ogrenci");
         }
-        public async Task<IActionResult> Index(){
-            var ogrenciler= await _context.Ogrenciler.ToListAsync();
-            return View(ogrenciler);
+        
+       
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id){
+            if(id==null)
+            return NotFound();
+            
+            var ogrenci=await _context.Ogrenciler.FindAsync(id);
+            if(ogrenci==null)
+            return NotFound();
+
+
+            return View(ogrenci);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id,Ogrenci model){
+            if(id!=model.OgrenciId)
+            return NotFound();
+            if(ModelState.IsValid){
+                try{
+                    _context.Update(model);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index","Ogrenci");
+                }
+                catch(DbUpdateConcurrencyException){
+                    if(!_context.Ogrenciler.Any(o=>o.OgrenciId==model.OgrenciId))
+                    return NotFound();
+                    throw;
+                }
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id){
+            
+            if(id==null)
+            return NotFound();
+            
+            var ogrenci=await _context.Ogrenciler.FindAsync(id);
+           
+            if(ogrenci==null)
+            return NotFound();
+
+            return View(ogrenci);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id){
+            var ogrenci=await _context.Ogrenciler.FindAsync(id);
+            if(ogrenci==null)
+            return NotFound();
+            _context.Ogrenciler.Remove(ogrenci);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index","Ogrenci");
+
+        }
+        
+
+      
     }
 }
--- efCoreApp/Data/Ogrenci.cs
+++ efCoreApp/Data/Ogrenci.cs
@@ -3,8 +3,11 @@ using System.ComponentModel.DataAnnotations;
 namespace efCoreApp.Data{
     public class Ogrenci{
         [Key]
+        
         public int OgrenciId { get; set; }
+        [Display(Name ="Ögrenci Adı")]
         public string? OgrenciAd { get; set; }
+        [Display(Name ="Ögrenci Soyadı")]
         public string? OgrenciSoyad { get; set; }
         public string? Eposta { get; set; }
         public string? Telefon { get; set; }
